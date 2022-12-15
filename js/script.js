bdb7d618af068fec14df1171ef8923b6e7b016ec
naykerman <112794205+naykerman@users.noreply.github.com>
2022-12-15 16:19:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,76 @@
+"use strict"
+//Синтаксис домашка
+/*
+//Варіант №1
+console.log('Вчимо JS')
+
+//Варіант №2
+console.log('JS')
+console.log('Вчимо')
+
+//Варіант №3
+console.log('Вчимо') console.log('JS')
+
+//варіант №4
+console.log('Вчимо');
+console.log('JS');
+*/
+
+
+//Змінні
+
+// 1)
+/*
+let userColorEyes;
+*/
+
+// 2)
+/*
+let user,
+   userName;
+userName = 'Вася';
+user = userName;
+console.log(user);
+*/
+
+// 3)
+/*
+//Варіант №1
+let user = 'Вася' let age = 36
+
+//Варіант №2
+let myage = 36;
+
+//Варіант №3
+const BLOCK_SIZE = 14 + 50;
+
+//Варіант №4
+const BLOCK_HEIGHT = 100;
+
+//Варіант №5
+if (true) {
+   var size = 15;
+}
+console.log(size);
+*/
+
+//Типи данних
+
+//Варіант №1
+let userAge = 36;
+let userInfo = "Фрілансер ${userAge}";
+
+//Варіант №2
+let userHeight = 145 / 0;
+//Вернеться значення NaN
+console.log(userHeight);
+
+//Варіант №3
+let userName;
+//Вернеться тип даних Null
+console.log(typeof userName);
+
+//Варіант №4
+let userSize = "45" / "8";
+//Вернеться тип данних Number
+console.log(typeof userSize);
